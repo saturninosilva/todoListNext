d51595c3a3dbc23c4dcc82e1fc748f6625792d4a
saturninosilva <100735469+saturninosilva@users.noreply.github.com>
2022-04-13 19:59:25 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,74 +1,24 @@
-import { useState, useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.css";
-
-import TodoList from "../src/TodoList";
-import TodoService from "../src/services/TodoService";
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Home() {
-  const [todoValue, setTodoValue] = useState("");
-  const [todoList, setTodoList] = useState([]);
-
-  useEffect(() => {
-    if (typeof window !== undefined) {
-      const todoService = new TodoService(window);
-      const todoListItem = todoService.getList();
-
-      try {
-        if (todoList.length == 0) {
-          const jsonList = JSON.parse(todoListItem);
-          setTodoList(jsonList);
-        }
-      } catch (e) {
-        console.log(`Erro no parse do storage: ${e}`);
-        todoService.save([]);
-      }
-    }
-  }, []);
-
-  useEffect(() => {
-    if (todoList.length > 0) {
-      const todoService = new TodoService(window);
-      todoService.save(todoList);
-    }
-  }, [todoList]);
-
-  const handleButtonClick = () => {
-    if (todoValue !== "") {
-      const todo = {
-        id: todoList.length + 1,
-        title: todoValue,
-        checked: false,
-      };
-
-      todoList.push(todo);
-
-      setTodoList([...todoList]);
-      setTodoValue("");
-    }
-  };
-
   return (
-    <div className="container">
+    <div className='container'>
       <div className="mb-3">
         <label htmlFor="input-tarefa" className="form-label"></label>
-        <input
-          type="text"
-          className="form-control"
-          id="input-tarefa"
-          placeholder="Digite sua tarefa"
-          onChange={(e) => setTodoValue(e.target.value)}
-          value={todoValue}
-        />
-        <button
-          type="submit"
-          className="btn btn-primary mt-3"
-          onClick={handleButtonClick}
-        >
-          Adicionar Tarefa
-        </button>
+        <input type="text" className="form-control" id="input-tarefa" placeholder="Digite sua tarefa" />
+        <button type="submit" className="btn btn-primary mt-3">Adicionar Tarefa</button>
       </div>
 
-      <TodoList list={todoList} />
+      <ul className="list-group">
+        <li className="list-group-item">
+          <div className="form-check">
+            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+              <label className="form-check-label" htmlFor="flexCheckDefault">
+                Default checkbox
+              </label>
+          </div>
+        </li>
+      </ul>
     </div>
-  );
+  )
 }
